ea7a45586d56247c36c7e285ac44424fee047d08
lbonnargent <lbonnargent@ateliericeberg.fr>
2015-04-04 01:03:24 +0200

    working version without tests

---
+++ README.md
@@ -0,0 +1,34 @@
+Installation
+===
+
+Sources
+---
+
+Clone this repos :
+
+`git clone -o github git@github.com:axlaurent/api-template.git`
+
+Install dependencies :
+
+`npm install`
+
+Configuration
+---
+
+Copy config.sample file :
+
+`cp config/config.sample config/config.json`
+
+Edit configuration file to match your needs :
+
+`editor config/config.json`
+
+Tests
+===
+
+Run `npm test`
+
+Start
+===
+
+Run `npm start`
---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var fs = require('fs'),
+    express = require('express'),
+
+    bodyParser = require('body-parser'),
+
+    server = {},
+    config = null,
+    app = null,
+    connection = null,
+    status = null;
+
+server.init = function() {
+  // Avoid initializing twice.
+  if (status !== null)
+    return;
+
+  // Loading configuration.
+  config = require('./config/config.json');
+
+  // creating express server.
+  app = express();
+
+  // Adding body-parser middleware to handle incomming JSON in request body.
+  app.use(bodyParser.json());
+
+  // routes definitions.
+  require('./src/routing.js')(app);
+
+  status = 'initialized';
+};
+
+server.start = function() {
+  // Do not start without configuration and routing.
+  if (!(status === 'initialized' || status === 'stopped'))
+    server.init();
+
+  // Starting to listen.
+  connection = app.listen(config.port);
+  console.log('Started server :', config.host, config.port.toString());
+
+  // Prevent from server rage quit on exception.
+  connection.on('error', function(err) {
+    log.error(err.code, err.message, err.details, err.context);
+  });
+  process.on('uncaughtException', function(err) {
+    log.error(err.code, err.message, err.details, err.context);
+  });
+
+  status = 'started';
+};
+
+server.stop = function() {
+  connection.close();
+  console.log('Stopped server :', config.host, config.port.toString());
+
+  status = 'stopped';
+};
+
+
+module.exports = server;
